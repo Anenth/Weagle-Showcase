e847cf2ae5106158b8296cbbb53985008b0bdf3e
Anenth <anenthvishnu@gmail.com>
2013-10-12 19:26:37 +0530

    Contact form completed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,35 @@
 'use strict';
-       $(document).ready(function(){
-           $('.main').onepage_scroll({
-               sectionContainer: 'section'
-           });
-           $('.nav>li>a').on('click',function(e){
-               e.preventDefault();
-               var selected = $(this);
-               $('.nav>li>a').removeClass('active');
-               selected.addClass('active');
-               $('.onepage-pagination>li>a[data-index='+ selected.data('index') +']').trigger('click');
-           });
-       });
+$(document).ready(function(){
+	$('.main').onepage_scroll({
+		sectionContainer: 'section'
+	});
+	$('.nav>li>a').on('click',function(e){
+		e.preventDefault();
+		var selected = $(this);
+		$('.nav>li>a').removeClass('active');
+		selected.addClass('active');
+		$('.onepage-pagination>li>a[data-index='+ selected.data('index') +']').trigger('click');
+	});
+
+	$('#ajax-contact-form').submit(function(e) {
+		e.preventDefault();
+
+		Parse.initialize('6qhUFEXtzdSTLwzG9VMeCU5oFaTWtgelh61unUiw', 'cTi3fECgna7cBjeYdyNIA6qnjehp1O8Ldrs8c5pe');
+		var Message = Parse.Object.extend('Messages_From_Showcase');
+		var message = new Message();
+		message.save({
+			name: $('#inputName').val(),
+			email: $('#inputEmail').val(),
+			subject: $('#inputSubject').val(),
+			message: $('#inputMessage').val(),
+		 },{
+		  success: function(object) {
+		    $('#note').html('<div>Your message has been sent. Thank you!</div>');
+		    $('#ajax-contact-form').hide();
+		  },error: function() {
+		  	$('#note').html('<div class="notification_error">Sorry, there is some technical issue with the server <br> Please Try again later</div>');
+		  }
+		});
+	});
+
+});
